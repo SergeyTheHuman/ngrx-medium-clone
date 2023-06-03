43c737c221563e40014e2fc7da10e7e3a5b2a7cd
Sergey <sergey2611.ivanov@yandex.ru>
2023-06-03 13:42:28 +0600

    (feature) added auth register form

---
+++ src/app/auth/components/auth-register-form/auth-register-form.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+
+@Component({
+	selector: 'mc-auth-register-form',
+	templateUrl: './auth-register-form.component.html',
+	styleUrls: ['./auth-register-form.component.scss'],
+})
+export class AuthRegisterFormComponent {
+	form!: FormGroup
+
+	constructor() {}
+
+	ngOnInit(): void {
+		this.initializeForm()
+	}
+	
+	initializeForm(): void {
+		this.form = new FormGroup({
+			username: new FormControl<string>('', [
+				Validators.required,
+				Validators.minLength(3),
+			]),
+			email: new FormControl<string>('', [
+				Validators.required,
+				Validators.minLength(3),
+				Validators.email,
+			]),
+			password: new FormControl<string>('', [
+				Validators.required,
+				Validators.minLength(3),
+			]),
+		})
+	}
+
+	onSubmit(): void {
+		console.log(this.form.value)
+	}
+}
