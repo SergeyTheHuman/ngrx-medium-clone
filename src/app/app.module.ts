a267e1c313b9484f4b156b5cdd5dcff3cb18314f
Sergey <sergey2611.ivanov@yandex.ru>
2023-06-03 14:12:14 +0600

    (feature) added ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { StoreModule } from '@ngrx/store'
 import { AppComponent } from '@src/app/app.component'
 import { AppRouting } from '@src/app/app.routing'
 import { AuthModule } from '@src/app/auth/auth.module'
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRouting, AuthModule],
+	imports: [BrowserModule, AppRouting, AuthModule, StoreModule.forRoot({}, {})],
 	providers: [],
 	bootstrap: [AppComponent],
 })
