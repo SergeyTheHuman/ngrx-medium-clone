b7658432dabc1d20cbdd00d386eac32273d388ca
Sergey <sergey2611.ivanov@yandex.ru>
2023-06-03 14:25:25 +0600

    (feature) added ngrx devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core'
+import { isDevMode, NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { AppComponent } from '@src/app/app.component'
 import { AppRouting } from '@src/app/app.routing'
 import { AuthModule } from '@src/app/auth/auth.module'
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRouting, AuthModule, StoreModule.forRoot({}, {})],
+	imports: [
+		BrowserModule,
+		AppRouting,
+		AuthModule,
+		StoreModule.forRoot({}, {}),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
